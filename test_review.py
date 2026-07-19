11d798a9f8d5ceec59a65478cff21ef4033f54ac
Yaroslav04 <miroshnichenko.jarik@gmail.com>
2026-07-19 20:03:43 +0300

    Test 5: testing markdown formatting fix

--- test_review.py
+++ test_review.py
@@ -1,11 +1,10 @@
 import fastapi
 
 def test():
-async def test():
     await db.query()
     
 async def test1():
-          time.sleep(15)
+    await asyncio.sleep(15)
 
 # Додаємо нову функцію для перевірки фіксу маркдауну
 def test2():
