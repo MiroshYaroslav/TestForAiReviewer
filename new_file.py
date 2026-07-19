9c04593e4a885a5a47225eca467ed25355c8b8c3
Yaroslav04 <miroshnichenko.jarik@gmail.com>
2026-07-19 21:26:41 +0300

    test7

---
+++ new_file.py
@@ -0,0 +1,13 @@
+import fastapi
+
+def test():
+    await db.query()
+    
+async def test1():
+    await asyncio.sleep(15)
+
+# Додаємо нову функцію для перевірки фіксу маркдауну
+def test2():
+          await db.query()
+
+# тест фіксу URL
