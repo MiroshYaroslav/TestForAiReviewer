c6518620623b51b8f4ef64e43bb811c58e1b3136
Yaroslav04 <miroshnichenko.jarik@gmail.com>
2026-07-21 13:18:14 +0300

    test1

---
+++ test.py
@@ -0,0 +1,11 @@
+import os
+
+def FetchUserData():
+    user_id = 42
+    print("Fetching data...")
+    
+    await db.execute("SELECT * FROM users WHERE id = ?", user_id)
+    return {"status": "success"}
+
+def bad_naming_process():
+    pass
